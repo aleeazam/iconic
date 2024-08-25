5d06bfdbdedf66a7581fe660b32eb58a372c36bd
Ali Azam <94777254+agzg@users.noreply.github.com>
2024-08-25 20:37:49 +0500

    plotter function made, ```square```

--- iconic.py
+++ iconic.py
@@ -2,14 +2,23 @@ import random
 
 from PIL import Image, ImageDraw
 
-def identicon():
-    width, pad = 256, 0.2 # icon's width and blank border
+def square(image, x, y, block, pad, colour):
+    x = x * block + pad
+    y = y * block + pad
+
+    draw = ImageDraw.Draw(image)
+    draw.rectangle((x, y, x + block, y + block), fill=colour)
+
+def identicon(seed):
+    if len(seed) != 15:
+            print("Seed needs to be 15 characters!")
+            raise TypeError
+    
+    width, pad = 256, 0.1 # icon's width and blank border
 
     p = int(width * pad)
     b = (width - 2 * p) // 5
     w = b * 5 + 2 * p
-
-    seed = "blah blah" # needs to be random or provided?
 
     lum = 40 + int(seed[0], 16)
     hue = int(seed[-6:], 16) / 0xffffff * 360
@@ -19,7 +28,10 @@ def identicon():
     colour = hsl
 
     # algorithmcally plot dots on the image like github user icons
+    # random for now
+    for i in range(3):
+            square(image, i, 0, b, p, colour)
 
     return image
 
-identicon().show()
+identicon("arcadeisthebest").show()
