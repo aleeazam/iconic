d7a659f9a8167784a88e0be9435f97f6428004f1
Ali Azam <94777254+agzg@users.noreply.github.com>
2024-08-26 17:49:12 +0500

    add more hashers; bugfix; done and dusted!

--- iconic.py
+++ iconic.py
@@ -1,59 +1,73 @@
 import random
 
 from PIL import Image, ImageDraw
-from hashlib import md5
+from hashlib import md5, sha1, blake2b
+from string import hexdigits
 
-md5 = lambda x: md5(x.encode()).hexdigest()
+_MD5   = lambda x: md5(x.encode()).hexdigest()
+_SHA   = lambda x: sha1(x.encode()).hexdigest()
+_BLAKE = lambda x: blake2b(x.encode()).hexdigest()
 
 def _square(image, x, y, block, pad, colour):
     x = x * block + pad
     y = y * block + pad
-
+	
     draw = ImageDraw.Draw(image)
     draw.rectangle((x, y, x + block, y + block), fill=colour)
 
-def identicon(seed, width=512, pad=0.1, invert=False, hasher=md5):
+def identicon(seed, width=420, pad=0.12, invert=False, hasher='md5'):
     """
-    Args:
-        seed (str): Seed used to generate the identicon.
-        width (int, optional): The width of the image in pixels.
-        pad (float, optional): Percentage border (of block) around the sprite.
-        invert(bool, optional): Invert the colour of the identicon?
-        hasher(optional): Function used to hash given seed. Leave blank for MD5.
-    Returns:
-        image: PIL.Image
-    ```
-    # Example
-    generate("seed").show()
-    generate("agzg").save("agzg.png")
+    ## Example:
+    generate("hakuum").show()
+    generate("arcade").save("arcade.png")
     ```
     """
 
     if pad <= 0.0 or pad > 0.4:
-        raise ValueError("0.0 <= pad < 0.4 only")
+        raise ValueError("0.0 < pad < 0.4 only")
 
+    if type(hasher) == str:
+        hasher = hasher.lower().strip()
+
+        if hasher == 'sha':
+            hasher = _SHA
+        elif hasher == 'blake':
+            hasher = _BLAKE
+        elif hasher == 'md5':
+            hasher = _MD5
+        else:
+            raise ValueError("hasher = 'md5', 'blake' or 'sha'")
+    else:
+        # Ensure custom hashers are compatible with algorithm.
+        # Otherwise, warn and switch to MD5.
+        test = hasher("test")
+        if type(test) != str or len(test) < 15 or not all(c in hexdigits for c in test):
+            raise ValueError("Provided hasher is incompatible")
+    
     seed = hasher(seed)[-15:]
-
+ 
     # Calculate image width, pixel size and padding.
     p = int(width * pad)
     b = (width - 2 * p) // 5
     w = b * 5 + 2 * p
 
-    lum = 40 + int(seed[0], 16)
+    # Use the seed to create HSL.
     hue = int(seed[-6:], 16) / 0xffffff * 360
-    hsl = f"hsl({int(hue)}, 80%, {lum}%)"
+    sat = 65 + int(seed[-2], 16) / 0xff * 25
+    lum = 45 + int(seed[0], 16) / 0xff * 40
+    hsl = f"hsl({hue}, {sat}%, {lum}%)"
 
     image  = Image.new("RGB", (w, w), "#F0F0F0")
     colour = hsl
-
+    
     if invert:
         image  = Image.new("RGB", (w, w), hsl)
         colour = "#F0F0F0"
-
+     
     filled = []
 
     for i, v in enumerate(seed):
-        yes = ord(v) % 2 != 0
+        yes = int(v, 16) <= 10
         filled.append(yes)
 
         if yes and i < 10:
@@ -62,24 +76,26 @@ def identicon(seed, width=512, pad=0.1, invert=False, hasher=md5):
         elif yes:
             _square(image, i // 5, i - 10, b, p, colour)
 
-    # Ignore completely filled or blank sprites.
-    if all(filled) or not any(filled):
-        return generate(seed, block, pad, invert, hasher)
-
+    # Ignore boring sprites.
+    if all(filled) or not any(filled) or sum(filled) < 3:
+        return identicon(seed, width, pad, invert, hasher)
+        
     return image
 
 if __name__ == "__main__":
+    VALID = "-_.() abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
     try:
         import argparse
     except:
         exit("pip install argparse")
-
+    
     parser = argparse.ArgumentParser()
     parser.add_argument("seed", nargs="?", help="Seed used to generate the identicon.")
     parser.add_argument("-i", "--invert", help="Invert identicon colours.", action="store_true")
     parser.add_argument("-s", "--save", help="Confirm save identicon.", action="store_true")
     parser.add_argument("-v", "--view", help="View identicon.", action="store_true")
-
+    
     args = parser.parse_args()
 
     if args.seed == None:
@@ -87,8 +103,9 @@ if __name__ == "__main__":
 
     image = identicon(args.seed, invert=args.invert)
 
-    if args.view:
-        image.show()
-
-    if args.save or input("save (y/N): ") == "y":
-        image.save(f"{args.seed}.png")
+    if args.view: image.show()
+    
+    if args.save or input("save (y/N): ").lower() in ("y", "yes"):
+        name = "".join([c if c in VALID else '-' for c in args.seed]) + ".png"
+        image.save(name)
+        print(f"Saved as {name}")
