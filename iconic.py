93c306186f7dbfdceb1a01ac3bfa9aa1fa0b14fb
Ali Azam <94777254+agzg@users.noreply.github.com>
2024-08-26 09:37:43 +0500

    add positional args

--- iconic.py
+++ iconic.py
@@ -13,10 +13,11 @@ def _square(image, x, y, block, pad, colour):
     draw = ImageDraw.Draw(image)
     draw.rectangle((x, y, x + block, y + block), fill=colour)
 
-def identicon(seed):
-    width, pad = 256, 0.1 # icon's width and blank border
-
-    seed = md5(seed)[-15:]
+def identicon(seed, width=512, pad=0.1, hasher=md5):
+    if pad < 0:
+        raise ValueError("pad >= 0.0")
+    
+    seed = hasher(seed)[-15:]
 
     p = int(width * pad)
     b = (width - 2 * p) // 5
@@ -29,15 +30,21 @@ def identicon(seed):
     image  = Image.new("RGB", (w, w), "#F0F0F0")
     colour = hsl
 
+    filled = []
+
     for i, v in enumerate(seed):
         yes = ord(v) % 2 != 0
+        filled.append(yes)
 
         if yes and i < 10:
-            _square(image, i // 5, i % 5, b, p, colour)        # first column
-            _square(image, 4 - i // 5, i % 5, b, p, colour)    # last column
+            _square(image, i // 5, i % 5, b, p, colour)        # 1
+            _square(image, 4 - i // 5, i % 5, b, p, colour)    # 3
         elif yes:
-            _square(image, i // 5, i - 10, b, p, colour)       # middle column
+            _square(image, i // 5, i - 10, b, p, colour)       # 2
 
+    if all(filled) or not any(filled):
+        return identicon(seed, block, pad, invert, hasher)
+        
     return image
 
 identicon("arcadeisthebest").show()
