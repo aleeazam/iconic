006c0d4ff9b533a36504f6e44c83dcb3c4926515
Ali Azam <94777254+agzg@users.noreply.github.com>
2024-08-26 10:39:12 +0500

    bugfix & add cmd args for interactive script

--- iconic.py
+++ iconic.py
@@ -4,7 +4,6 @@ from PIL import Image, ImageDraw
 from hashlib import md5, sha1
 
 md5 = lambda x: md5(x.encode()).hexdigest()
-sha1 = lambda x: sha1(x.encode()).hexdigest()
 
 def _square(image, x, y, block, pad, colour):
     x = x * block + pad
@@ -43,8 +42,32 @@ def identicon(seed, width=512, pad=0.1, hasher=md5):
             _square(image, i // 5, i - 10, b, p, colour)       # 2
 
     if all(filled) or not any(filled):
-        return identicon(seed, block, pad, invert, hasher)
+        return identicon(seed, block, pad, hasher) # ignore boring icons
+                                                   # blotted square or empty ones
         
     return image
 
-identicon("arcadeisthebest").show()
+if __name__ == "__main__":
+    try:
+        import argparse
+    except:
+        exit("pip install argparse")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("seed", nargs="?", help="Seed used to generate the identicon.")
+    parser.add_argument("-i", "--invert", help="Invert identicon colours.", action="store_true")
+    parser.add_argument("-s", "--save", help="Confirm save identicon.", action="store_true")
+    parser.add_argument("-v", "--view", help="View identicon.", action="store_true")
+
+    args = parser.parse_args()
+
+    if args.seed == None:
+        args.seed = input("Enter seed: ").strip()
+
+    image = identicon(args.seed, invert=args.invert)
+
+    if args.view:
+        image.show()
+
+    if args.save or input("save (y/N): ") == "y":
+        image.save(f"{args.seed}.png")
